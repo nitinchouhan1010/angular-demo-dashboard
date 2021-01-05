0c332764472942f536dd26d496062e0da47f2901
nitinchouhan1010 <nitinchouhan4444@gmail.com>
2021-01-05 12:00:40 +0530

    5 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { InventoryComponent } from './inventory/inventory.component';
-import { TIcketsComponent } from './tickets/tickets.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { InventoryComponent } from "./inventory/inventory.component";
+import { TIcketsComponent } from "./tickets/tickets.component";
+import { HeaderComponent } from "./header/header.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, InventoryComponent, TIcketsComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    InventoryComponent,
+    TIcketsComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
